16c837fc2c619df2f083e79fd60150fb7017d294
Mathis <mcapisano1@myges.fr>
2024-03-12 16:49:57 +0100

    Make the server decode the message

--- src/mathisServer.py
+++ src/mathisServer.py
@@ -1,5 +1,8 @@
 from scapy.all import *
 
 a = sniff(filter="icmp", count=2)
-a.nsummary()
-print(a[1])
+#a.nsummary()
+receivedMessage = a[1][3].fields.get("load").decode("utf-8")
+
+print("receivedMessage : "+receivedMessage)
+
