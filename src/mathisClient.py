ea275ce393e78d2f2b15e47e9a641b6e0b2205d2
Mathis <mcapisano1@myges.fr>
2024-04-29 16:51:35 +0200

    Transfer files

--- src/mathisClient.py
+++ src/mathisClient.py
@@ -1,3 +1,23 @@
 from scapy.all import sr1,IP,ICMP
+import base64
 
-p = sr1(IP(dst="www.slashdot.org")/ICMP()/"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000A")
+
+while True:
+
+	action = input("-----------------------\nWhat should we do (with a lazy sailor) ? \nActions are : file | message | command\n")
+	
+	if action == "file":
+		
+	
+		fileName = input("Which file should we send ?\n")
+
+		fileToSend = open(fileName,"rb")
+		binaryData = fileToSend.read()
+		encoded = (base64.b64encode(binaryData)).decode('ascii')
+
+
+		# Protocole : type d'envoi (file/message)
+		p = sr1(IP(dst="192.168.18.1")/ICMP()/"file")
+		p = sr1(IP(dst="192.168.18.1")/ICMP()/fileName)
+		p = sr1(IP(dst="192.168.18.1")/ICMP()/encoded)
+
--- src/mathisServer.py
+++ src/mathisServer.py
@@ -1,8 +1,24 @@
 from scapy.all import *
+import base64
 
-a = sniff(iface="VMware Network Adapter VMnet8", filter="icmp", count=2)
-#a.nsummary()
-receivedMessage = a[1][3].fields.get("load").decode("utf-8")
+sniffInterface = "VMware Network Adapter VMnet8"
 
-print("receivedMessage : "+receivedMessage)
 
+while True:
+    a = sniff(iface=sniffInterface, filter="icmp", count=1)
+    receivedMessage = a[0][3].fields.get("load").decode("utf-8")
+
+    print("receivedMessage : "+receivedMessage)
+
+    if receivedMessage == "file":
+        # Receive 3 ICMP as 2 firsts are going to be the request and reply from the file name
+        # Then, the 3rd one is going to be the request of the file 
+        icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=3)
+        
+        fileName = icmpReceived[0][3].fields.get("load").decode("utf-8")
+        base64File = icmpReceived[2][3].fields.get("load")
+
+        print("fileName : "+fileName)
+        print("base64File : "+base64File.decode("utf-8"))
+        with open(fileName, "wb") as fh:
+            fh.write(base64.decodebytes(base64File))
